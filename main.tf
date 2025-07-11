fe30552023bc803ea1169650a70768d4fe12d4ac
Yogendra Rathore <yogendra.rathore91@gmail.com>
2025-07-11 20:13:48 +0530

    new branch

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
-}
-#koi kisi ko kyn chahe 
+}#koi kisi ko kyn chahe 
 #hi who are you 
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
